2c4c9256a22b96ba7ca00493b2b63b25dab725a4
Andrew Guinness <aaguinness@gmail.com>
2017-12-04 11:21:03 -0500

    Updated translation

--- Translation.py
+++ Translation.py
@@ -1,29 +1,10 @@
-import sys
-import os
-import shutil
-import subprocess
-
-
-codontable = {
-    'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
-    'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
-    'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
-    'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
-    'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
-    'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
-    'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
-    'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
-    'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
-    'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
-    'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
-    'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
-    'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
-    'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
-    'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_',
-    'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W',
-    }
-
-
+codonmap = {}
+with open("codonmap.txt") as f:
+    for line in f:
+       (val, key) = line.split()
+       codonmap[key] = val
+  
+  
 
 def translate(seq):
     peptide = ''
@@ -32,15 +13,25 @@ def translate(seq):
     seqtrimmed = seq[int(start):]
     for n in range(0,len(seqtrimmed),3):
         codon = seqtrimmed[n:n+3]
-        aa = codontable.get(codon, '*')
-        if aa != '_':
+        aa = codonmap.get(codon, '*')
+        if aa != 'Stop':
             peptide += aa
         else:
             break
     return peptide
-
+    
 
 seqtest = 'ACTCCTCTGGAGCTGGATTGGGAGCTGAGTGGAGAAGAAGCCAGGACTCTCACTAGACCGTGAACCACAGTTGCTGCAATGGCCGGGAAGCCCGTGCTTCACTACTTCAATGCCCGGGGCAGAATGGAGTGCATCAGGTGGCTCCTGGCTGCAGCAGGGGTGGAGTTTGAAGAGAAGTTTATACAGAGTCCGGAAGATTTGGAAAAGCTAAAAAAAGATGGGAATTTGATGTTTGACCAAGTGCCCATGGTGGAGATTGATGGGATGAAGCTGGTGCAGACCAGAGCCATTCTCAACTACATCGCCACCAAATATGACCTCTATGGGAAGGACATGAAGGAGAGAGCCCTGATTGACATGTATACAGAAGGTATTTTAGATCTGACTGAAATGATTGGGCAATTGGTATTATGTCCCCCAGACCAAAGAGAAGCCAAGACTGCCTTGGCAAAAGACAGGACCAAAAACCGTTACTTGCCTGCCTTTGAAAAGGTGTTGAAGAGCCATGGACAAGACTACCTTGTGGGCAACAGGCTGACCAGGGTGGACGTCCACCTGCTGGAACTTCTTCTCTATGTTGAAGAGCTTGATGCCAGCCTTCTGACCCCTTTCCCTCTGCTGAAGGCCTTCAAGAGCAGAATCAGCAGCCTCCCCAATGTGAAGAAGTTCCTACATCCTGGCAGCCAGAGAAAGCCTCCCTTGGATGCAAAACAAATTGAAGAAGCAAGGAAGGTTTTCAAGTTTTAGTGTGGCTGCATTGATGGAGCCACAGATACTGGCCTCTAATTGTTTGCAATTATAAAATACAATTGTTGATTCTGGCTATTGTGCAATAATAAAAAATTAACAACTGG'
 
 translate(seqtest)
 
+# load fasta file
+fasta = open('control1.fasta',"r")
+
+# loop over the file
+for line in fasta:
+    line = line.strip()
+    if ">" in line:
+        seqid = line
+        print seqid
+    else: translate(line)
