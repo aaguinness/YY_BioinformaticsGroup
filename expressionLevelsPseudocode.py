2c392e0848e571c8fe7eb0d5bd8fd1f25199d01b
Katherine <bookaholic214@gmail.com>
2017-12-04 11:12:32 -0500

    Merging expression level pseudocode because Github is mad about something

--- expressionLevelsPseudocode.py
+++ expressionLevelsPseudocode.py
@@ -1,5 +1,5 @@
 from plotnine import *
-expression = ggplot(data=RNAseqfile1,aes(x="mouse type", color="red"))
+expression = ggplot(data=RNAseqfile1,aes(x="mouse type",color="red"))
     +geom_bar(RNAseqfile2,aes(x="mouse type",color="orange"))
     +geom_bar(RNAseqfile3,aes(x="mouse type",color="green"))
     +geom_bar(RNAseqfile4,aes(x="mouse type",color="blue"))
