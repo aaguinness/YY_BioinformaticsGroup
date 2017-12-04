abe582cd3661c54697d305a6c7b072db258aa433
omegadan01 <dbruzzes@nd.edi>
2017-12-04 10:57:47 -0500

    open text file

--- expressionLevelsPseudocode.py
+++ expressionLevelsPseudocode.py
@@ -3,4 +3,3 @@ expression = ggplot(data=RNAseqfile1,aes(x="mouse type", color="red"))
     +geom_bar(RNAseqfile2,aes(x="mouse type",color="orange"))
     +geom_bar(RNAseqfile3,aes(x="mouse type",color="green"))
     +geom_bar(RNAseqfile4,aes(x="mouse type",color="blue"))
-    
---
+++ translate_Dan.py
@@ -0,0 +1,10 @@
+import sys
+import os
+import shutil
+import subprocess
+import pandas as pd
+
+codon = pd.read_csv('codonmap.txt', delimiter="\t")
+
+
+
