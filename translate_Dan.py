19174159f51c8b5d691cb9acbeb04bbf544c29e5
omegadan01 <dbruzzes@nd.edi>
2017-12-04 11:12:42 -0500

    started fasta shite

--- translate_Dan.py
+++ translate_Dan.py
@@ -4,7 +4,39 @@ import shutil
 import subprocess
 import pandas as pd
 
-codon = pd.read_csv('codonmap.txt', delimiter="\t")
+# load codon table and turn it into a dictionary
+with open("codonmap.txt") as fin:
+    rows = (line.split('\t') for line in fin)
+    codon = {row[0]: row[1:] for row in rows}
 
 
+def translate(seq):
+    peptide = ''
+
+    start = seq.find('ATG')
+    seqtrimmed = seq[int(start):]
+    for n in range(0, len(seqtrimmed), 3):
+        codon = seqtrimmed[n:n + 3]
+        aa = codontable.get(codon, '*')
+        if aa != '_':
+            peptide += aa
+        else:
+            break
+    return peptide
+
+
+
+
+
+
+
+# load fasta file
+fasta = open('control1.fasta',"r")
+
+# loop over the file
+for line in fasta:
+    line = line.strip()
+    if ">" in line:
+        seqid = line
+        print seqid
 
