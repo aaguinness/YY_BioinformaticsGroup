474d4ed8e82f6ecd9ed1f6d968a6efaac662f1de
Katherine <bookaholic214@gmail.com>
2017-12-05 15:28:24 -0500

    Final

--- expressionLevelsPseudocode.py
+++ expressionLevelsPseudocode.py
@@ -1,5 +1,14 @@
+import pandas
 from plotnine import *
-expression = ggplot(data=RNAseqfile1,aes(x="mouse type",color="red"))
-    +geom_bar(RNAseqfile2,aes(x="mouse type",color="orange"))
-    +geom_bar(RNAseqfile3,aes(x="mouse type",color="green"))
-    +geom_bar(RNAseqfile4,aes(x="mouse type",color="blue"))
+Transcript1 = pandas.read_csv("1.sequences.table")
+###somehow add column titles so ggplot knows what to do
+Transcript2 =
+Transcript6 = 
+Transcript9 = 
+Transcript10 =
+expression = ggplot(data=Transcript1,aes(x="",color="red"))
+    +geom_bar(Transcript2,aes(x="mouse type",color="orange"))
+    +geom_bar(Transcript3,aes(x="mouse type",color="green"))
+    +geom_bar(Transcript4,aes(x="mouse type",color="blue"))
+    +geom_bar(Transcript5,aes(x="mouse type",color="blue"))
+    +geom_bar(Transcript6,aes(x="mouse type",color="blue"))
