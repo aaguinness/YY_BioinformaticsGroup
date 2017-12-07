ed7070e9a4eda739ee301bae7cfe1428611ba361
Katherine <bookaholic214@gmail.com>
2017-12-07 16:46:00 -0500

    Updated with successful graphs for Transcripts 6 and 9

--- expressionLevels/expressionLevels.py
+++ expressionLevels/expressionLevels.py
@@ -1,5 +1,5 @@
 #load packages
-import pandas as pd
+import pandas
 from plotnine import *
 
 #read in hmm search output files and add column names for ggplot
@@ -7,14 +7,10 @@ Transcript1 = pd.read_csv("1.sequences.table", sep=' ', header=None)
 Transcript1.columns = ["Mouse", "Match sequence", "Score"]
 Transcript2 = pd.read_csv("2.sequences.table", sep=' ', header=None)
 Transcript2.columns = ["Mouse", "Match sequence", "Score"]
-#note: there were no matches for Transcript 6 in any mouse
-#Thus insufficient data/no expression for Ptpn5
 Transcript6 = pd.read_csv("6.sequences.table", sep=' ', header=None)
 Transcript6.columns = ["Mouse", "Match sequence", "Score"]
 Transcript8 = pd.read_csv("8.sequences.table", sep=' ', header=None)
 Transcript8.columns = ["Mouse", "Match sequence", "Score"]
-#note: there were no matches for Transcript 9 in any mouse
-#Thus insufficient data/no expression for Lhx2
 Transcript9 = pd.read_csv("9.sequences.table", sep=' ', header=None)
 Transcript9.columns = ["Mouse", "Match sequence", "Score"]
 Transcript10 = pd.read_csv("10.sequences.table", sep=' ', header=None)
@@ -31,19 +27,30 @@ expressionT2 = (ggplot(Transcript2,aes(x="Mouse"))
     +ggtitle("Scl7a12")
     +ylab("Relative Expression")
     +theme_classic())
+expressionT6 = (ggplot(Transcript6,aes(x="Mouse"))
+    +geom_bar(fill='yellow')
+    +ggtitle("Ptpn5")
+    +ylab("Relative Expression")
+    +theme_classic())
 expressionT8 = (ggplot(Transcript8,aes(x="Mouse"))
     +geom_bar(fill='green')
     +ggtitle("ATP12a")
     +ylab("Relative Expression")
     +theme_classic())
-expressionT10 = (ggplot(Transcript10,aes(x="Mouse"))
+expressionT9 = (ggplot(Transcript9,aes(x="Mouse"))
     +geom_bar(fill='blue')
+    +ggtitle("Lhx2")
+    +ylab("Relative Expression")
+    +theme_classic())
+expressionT10 = (ggplot(Transcript10,aes(x="Mouse"))
+    +geom_bar(fill='purple')
     +ggtitle("Synpr")
     +ylab("Relative Expression")
     +theme_classic())
 print(expressionT1)
 print(expressionT2)
+print(expressionT6)
 print(expressionT8)
+print(expressionT9)
 print(expressionT10)
-
 
