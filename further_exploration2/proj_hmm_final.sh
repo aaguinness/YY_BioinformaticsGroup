511a0116e8ca87ec1af5f63f18a9c7ef3f8fc74a
omegadan01 <dbruzzes@nd.edi>
2017-12-07 19:25:05 -0500

    added Balaji's shell script

---
+++ further_exploration2/proj_hmm_final.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+for f in *.sequences.fasta
+do
+    ../../muscle3.8.31_i86darwin64 -in $f -out ${f%.fasta}.align   
+    echo $f 
+done
+
+for f in *.align
+do
+
+	../../hmmbuild ${f%.align}.hmm $f
+	echo $f
+done
+
+for f in *.hmm
+do
+    for fasta_file in ../RNA_seq_translation/*_translated.fasta
+    do
+	    ../../hmmsearch --tblout ${f%.hmm}.hits $f $fasta_file
+	    echo "$f"
+	    cat ${f%.hmm}.hits | grep -v "#" | awk '{print $1,$3,$5}' >> ${f%.hmm}.table
+    done
+done
+
+mkdir align/; mv *.align $_
+mkdir hmm/; mv *.hmm $_
+mkdir hits_table/; mv *.hits *.table $_
+
+
